44de19681e86d2aa5089874c10e986ebd162c347
Denis <xdenis@yandex.ru>
2022-09-26 21:13:45 +0300

    Решение задач с числами 4

---
+++ Desktop/Lesson C#/z1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 24: Напишите программу, которая
+// 1.принимает на вход число (А) и 
+// 2.выдаёт сумму чисел от 1 до А
+
+
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+//1 + 2 + 3 + 4 + 5 + 6 + 7
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumNumbers = SumNumbers2(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sumNumbers}");
+
+//    int SumNumbers2(int num)
+//  {
+//       int sum = default; 
+//     for (int i = 1; i <= num; i++)
+//      {
+//          sum = sum + i; 
+//      }
+//      return sum;
+//  }
+//  Console.WriteLine();
+ 
+ int SumNumbers2 (int num)
+ {
+    int sum = default;
+    int counter = 1;
+    while (counter <= num)
+    {
+        //sum = sum + counter;
+        sum += counter;
+        counter++;
+     }
+    return sum;
+}
+
+
+
+
