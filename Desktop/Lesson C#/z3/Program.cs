390150b778ac9c299204bc72a5241551335bc36d
Denis <xdenis@yandex.ru>
2022-09-26 22:06:44 +0300

    Решение задач с числами  задача3

---
+++ Desktop/Lesson C#/z3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] array = new int[8];
+
+FillArrayRandom();
+PrintArray();
+
+void FillArrayRandom()
+{
+    for (int i = 0; i <= array.Length - 1; i++) array[i] = new Random().Next(0, 2);
+}
+
+void PrintArray()
+{
+    Console.WriteLine($"[");
+    for (int i = 0; i <= array.Length - 2; i++) Console.Write($"{array[i]},");
+    Console.WriteLine($"{array[array.Length - 1]}]");
+}
+
