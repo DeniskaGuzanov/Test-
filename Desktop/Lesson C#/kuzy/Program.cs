7a27220074e098572d2f38c46122c07ec80eaf7e
Denis <xdenis@yandex.ru>
2022-10-10 20:02:24 +0300

    kuzy

---
+++ Desktop/Lesson C#/kuzy/Program.cs
@@ -0,0 +1,14 @@
+﻿int [,] array = new int [3, 3];
+void PrintArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+         arr[i, j] = new Random().Next(-10, 10);       
+         Console.Write($"{arr[i, j]}, ");
+        }
+        Console.WriteLine(); 
+    }
+}   
+PrintArr(array);
