9e36aa054dce4418ee6ab84370e7dfe95c3f3c0b
Denis <xdenis@yandex.ru>
2022-10-13 20:21:10 +0300

    Задача 60

---
+++ Desktop/Lesson C#/homework60/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 60. 
+// ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+//  Напишите программу, которая будет построчно выводить массив,
+//  добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
+// 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
+
+
+int x = 2;
+int y = 2;
+int z = 4;
+Console.WriteLine($"");
+
+int[,,] array3D = new int[x, y, z];
+CreateArray(array3D);
+WriteArray(array3D);
+
+void WriteArray (int[,,] arr3D)
+{
+  for (int i = 0; i < arr3D.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr3D.GetLength(1); j++)
+    {
+      
+      for (int k = 0; k < arr3D.GetLength(2); k++)
+      {
+        Console.Write( $"{arr3D [i, j, k]} = ({i},{j},{k}) ");
+      }
+      Console.WriteLine();
+    }
+    Console.WriteLine();
+  }
+}
+
+void CreateArray(int[,,] arr3D)
+{
+  int[] temp = new int[arr3D.GetLength(0) * arr3D.GetLength(1) * arr3D.GetLength(2)];
+  int  number;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 100);
+    number = temp[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 100);
+          j = 0;
+          number = temp[i];
+        }
+          number = temp[i];
+      }
+    }
+  }
+  int count = 0; 
+  for (int x = 0; x < arr3D.GetLength(0); x++)
+  {
+    for (int y = 0; y < arr3D.GetLength(1); y++)
+    {
+      for (int z = 0; z < arr3D.GetLength(2); z++)
+      {
+        arr3D[x, y, z] = temp[count];
+        count++;
+      }
+    }
+  }
+}
